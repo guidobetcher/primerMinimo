f89a9c9e04f3fc89926971cb5af90311004f132a
Joseph <joseph.zandueta@estudiante.upc.edu>
2019-11-07 09:41:23 +0100

    He creado una rama para que

--- primerMinimo/src/main/java/ProductManager.java
+++ primerMinimo/src/main/java/ProductManager.java
@@ -1,7 +1,18 @@
 public interface ProductManager {
+
+    //Listado ordenado ascendente de productos por precio
     void sortProducts (Product[] pList);
+
+    //Realiza un nuevo pedido asociado a un usuario
     void newOrder (Order order, User user);
+
+    //Sirve el pedido, es una cola?
     void serveOrder (Order[] waitingOrders);
+
+    //Listado de pedidos que han sido realizados
+    //Debe haber un historico de pedidos
     Order listOrder(User user);
+
+    //Listado de productos ordenado de mas a menos ventas
     void productList();
 }
